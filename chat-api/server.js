f1ca4d1426cf3201a102aa8e091200898a5c6498
Vinnick <romanvinnick@gmail.com>
2021-12-15 22:13:23 +0100

    initial

---
+++ chat-api/server.js
@@ -0,0 +1,44 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+let users = []
+let messages = []
+let index = 0
+
+io.on('connection', (socket) => {
+  socket.emit('loggedIn', {
+    users: users.map((s) => s.username),
+    messages: messages,
+  })
+
+  socket.on('newuser', (username) => {
+    console.log(`${socket.username} has entered the party`)
+    socket.username = username
+    users.push(socket)
+
+    io.emit('userOnline', socket.username)
+  })
+
+  socket.on('msg', (msg) => {
+    let message = {
+      index: index,
+      username: socket.username,
+      msg: msg,
+    }
+    messages.push(message)
+    io.emit('msg', message)
+    index++
+  })
+
+  //disconnect
+  socket.on('disconnect', () => {
+    console.log(`${socket.username} has left the party`)
+    io.emit('userLeft', socket.username)
+    users.splice(users.indexOf(socket), 1)
+  })
+})
+
+http.listen(process.env.port || 3000, () => {
+  console.log('listening on port %s', process.env.port || 3000)
+})
---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="columns">
+      <div class="column green"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client'
+export default {
+  name: 'app',
+  components: {},
+  data: function (){
+    return {
+      username: '',
+      socket: io('http://localhost:3000'),
+      messages:[],
+      users:[]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
