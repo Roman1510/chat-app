42c0b788cfac7b7178c7e613f9cf2b24289d3c12
Roman1510 <romanvinnick@gmail.com>
2021-12-23 19:12:27 +0100

    fixing sass issue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,42 @@
 <template>
-  <div id="app">
-    <div class="columns">
-      <div class="column green">
-        <p>Username: {{ username }}</p>
-        <p>Online:{{ users.length }}</p>
-        <ChatRoom :messages="messages" @sendMessage="sendMessage" />
-      </div>
-    </div>
-  </div>
+	<div class="hero is-fullheight">
+		<div class="hero-body">
+			<p>Username: {{ username }}</p>
+			<p>Online:{{ users.length }}</p>
+			<ChatRoom :messages="messages" @sendMessage="sendMessage" />
+		</div>
+	</div>
+	<div class="modal" :class="{ 'is-active': modalIsActive }">
+		<div class="modal-background"></div>
+		<div class="modal-card">
+			<header class="modal-card-head">
+				<p class="modal-card-title">Please fill in the name:</p>
+				<button class="delete" aria-label="close"></button>
+			</header>
+			<section class="modal-card-body">
+				<div class="columns is-centered">
+					<div class="column is-half">
+						<input class="input" type="text" v-model="username" />
+					</div>
+				</div>
+			</section>
+			<footer class="modal-card-foot">
+				<button class="button is-success" @click.prevent="changeUser">
+					Save changes
+				</button>
+				<button class="button" @click.prevent="cancelModal">Cancel</button>
+			</footer>
+		</div>
+	</div>
 </template>
 
 <script>
 import ChatRoom from './components/ChatRoom.vue'
 import { sendSocket, loggedIn } from './utilities/SocketConnect.js'
+import "../node_modules/bulma/sass/elements/button.sass";
+
 export default {
-  /* 
+	/* 
   1. show the list of users
   2. make new styling
   2.1 make styling for modal prompt + localstorage to store the user's info (at least the name, that the particular name is online)
@@ -30,42 +52,42 @@ export default {
     2)put socket operations in a separate file
     3)update the variables from the external file reactively
   */
-  name: 'app',
-  components: { ChatRoom },
-  data: function () {
-    return {
-      username: '',
-      messages: [],
-      users: [],
-    }
-  },
-  methods: {
-    sendMessage: function (message) {
-      sendSocket(message)
-    },
-    updateDataWS: function (messages, users) {
-      this.messages = [...messages]
-      this.users = [...users]
-    },
-  },
-  mounted: function () {
-    if (!this.username) {
-      this.username = prompt('what is your username', 'anonymous')
-    }
-    loggedIn(this.username, (messages, users) => {
-      this.updateDataWS(messages, users)
-    })
-  },
+	name: 'app',
+	components: { ChatRoom },
+	data: function () {
+		return {
+			username: '',
+			messages: [],
+			users: [],
+			modalIsActive: false,
+		}
+	},
+	methods: {
+		sendMessage: function (message) {
+			sendSocket(message)
+		},
+		updateDataWS: function (messages, users) {
+			this.messages = [...messages]
+			this.users = [...users]
+		},
+	},
+	mounted: function () {
+		loggedIn(this.username, (messages, users) => {
+			this.updateDataWS(messages, users)
+		})
+	},
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
+$button-color: hsl(0.3, 0%, 21%)
+
 </style>
