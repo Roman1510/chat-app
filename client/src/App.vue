073da7a3d9dccdd566298f29dd2c7e3da1fd0a43
Vinnick <romanvinnick@gmail.com>
2022-01-01 23:10:40 +0100

    cleaning

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,65 +1,9 @@
 <template>
-	<form class="box">
-		<div class="field">
-			<label class="label"
-				>Username: {{ username && !modalIsActive ? username : '' }}</label
-			>
-		</div>
-		<div class="field">
-			<label class="label">Online:{{ users.length }}</label>
-		</div>
-		<button
-			class="button is-primary"
-			@click.prevent="username ? signOut() : openModal()"
-		>
-			{{ username ? 'Log out' : 'Log in' }}
-		</button>
-	</form>
-	<ChatRoom :messages="messages" @sendMessage="sendMessage" />
-	<div class="modal" :class="{ 'is-active': modalIsActive }">
-		<div class="modal-background"></div>
-		<div class="modal-card">
-			<header class="modal-card-head">
-				<p class="modal-card-title">Please fill in the name:</p>
-				<button
-					class="delete"
-					@click.prevent="closeModal"
-					aria-label="close"
-				></button>
-			</header>
-			<section class="modal-card-body">
-				<div class="columns is-centered">
-					<div class="column is-half">
-						<input class="input is-primary" type="text" v-model="username" />
-					</div>
-				</div>
-			</section>
-			<footer class="modal-card-foot">
-				<button class="button is-success" @click.prevent="setUsername">
-					Save changes
-				</button>
-				<button class="button" @click.prevent="closeModal">Cancel</button>
-			</footer>
-		</div>
-	</div>
+<ChatRoom/>
 </template>
 
 <script>
-import ChatRoom from './components/ChatRoom.vue'
-import {
-	sendSocket,
-	loggedIn,
-	leaveParty,
-	establishWS,
-} from './utilities/SocketConnect.js'
-import {
-	setCurrentUser,
-	getCurrentUser,
-	removeUser,
-} from './utilities/StoreUser'
-
-export default {
-	/* 
+  /* 
   1. show the list of users
   2. make new styling
 
@@ -78,77 +22,17 @@ export default {
     3)update the variables from the external file reactively
   */
 
-	name: 'app',
-	components: { ChatRoom },
-	data: function () {
-		return {
-			username: getCurrentUser(),
-			messages: [],
-			users: [],
-			modalIsActive: false,
-		}
-	},
-	methods: {
-		initialize: function () {
-			console.log('initialize was called', this.username,this.users)
-
-			if (this.username) {
-				loggedIn(this.username, (messages, users) => {
-					this.updateDataWS(messages, users)
-				})
-			}
-			setTimeout(()=>{
-				establishWS()
-			},1000)
-		},
-		sendMessage: function (message) {
-			sendSocket(message)
-		},
-		updateDataWS: function (messages, users) {
-			this.messages = [...messages]
-			this.users = [...users]
-		},
-		getUsername: function () {
-			const currentUser = getCurrentUser()
-			if (currentUser !== this.username) {
-				this.username = currentUser
-			}
-		},
-		signOut: function () {
-			this.username = ''
-			this.messages = []
-			this.users=[]
-			removeUser()
-			leaveParty()
-		},
-		setUsername: function () {
-			if (this.username) {
-				this.initialize()
-			}
-			setCurrentUser(this.username)
-			this.closeModal()
-		},
-		// modal
-		closeModal: function () {
-			this.modalIsActive = false
-		},
-		openModal: function () {
-			this.modalIsActive = true
-		},
-	},
-	mounted: function () {
-		if (!this.username) {
-			this.modalIsActive = true
-		} else {
-			this.initialize()
-		}
-	},
+import ChatRoom from './components/ChatRoom.vue'
+export default {
+  name: 'app',
+  components: { ChatRoom },
 }
+
 </script>
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
