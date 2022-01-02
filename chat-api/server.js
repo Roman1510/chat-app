b47ca42511afc108c2731b1394cc94f60cf33e2b
Vinnick <romanvinnick@gmail.com>
2022-01-02 23:48:51 +0100

    added append,remove user functionality

--- chat-api/server.js
+++ chat-api/server.js
@@ -10,17 +10,30 @@ const io = require('socket.io')(http, {
 const messages = []
 const users = []
 
+/*
+  1. append new user on new login
+  2. remove user on every disconnect
+
+*/
+
 io.on('connection', (socket) => {
   socket.on('newUser', (user) => {
+    users.push(user)
+    socket.username = user
     //message to a current client
-    socket.emit('loggedIn', {msg:`WELCOME ${user}!`})
+    socket.emit('loggedIn', { msg: `WELCOME ${user}!` })
     //message to all the clients except the current
-    socket.broadcast.emit('loggedIn', {msg:`${user} joined the chat.`})
+    socket.broadcast.emit('loggedIn', { msg: `${user} joined the chat.` })
     //message to all the clients (just in case)
   })
 
   socket.on('disconnect', () => {
-    io.emit('message', {msg:'A user has left the chat'})
+    console.log(`${socket.username} left`)
+    var toDelete = users.indexOf(socket.user)
+    if (toDelete !== -1) {
+      users.splice(toDelete, 1)
+    }
+    io.emit('message', { msg: `A user ${socket.username} has left the chat` })
   })
 
   socket.on('chatMessage', (msg) => {
