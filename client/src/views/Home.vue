ba9fb21cc62e64223035d53f8a816b2787fb24fb
Vinnick <romanvinnick@gmail.com>
2022-01-03 15:02:43 +0100

    added vue-router

---
+++ client/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <ChatRoom />
+</template>
+
+<script>
+import ChatRoom from '@/components/ChatRoom.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ChatRoom,
+  },
+}
+</script>
