fa0bfbe6f2dade47e4ee00bb04f03aab431bfda8
Vinnick <romanvinnick@gmail.com>
2022-01-03 17:27:28 +0100

    backup before restyling

--- client/src/components/UserLogin.vue
+++ client/src/components/UserLogin.vue
@@ -1,13 +1,28 @@
-<template lang="">
-    <div>
-        
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-head">
+      <header class="hero is-link is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <p class="title">Developer's lair</p>
+            <p class="subtitle">built with Vue 3 by @romanXVX</p>
+          </div>
+        </div>
+      </header>
     </div>
+    <div class="field has-addons my-5 mx-5">
+      <div class="control is-expanded">
+        <input class="input" type="text" placeholder="Type your message" />
+      </div>
+      <div class="control">
+        <button class="button is-info">Send</button>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
-    name:'UserLogin'
+  name: 'UserLogin',
 }
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <UserLogin/>
+  </div>
+</template>
+<script>
+import UserLogin from "../components/UserLogin.vue";
+export default {
+    components: { UserLogin }
+}
+</script>
+<style lang="">
+  
+</style>
