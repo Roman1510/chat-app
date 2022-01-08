16a9a84266b0e4dd3bd4effa5e586bec9c553b5d
Vinnick <romanvinnick@gmail.com>
2022-01-08 23:51:12 +0100

    added the right css configuration for the chat box

--- client/src/components/ChatRoom.vue
+++ client/src/components/ChatRoom.vue
@@ -102,72 +102,27 @@ export default {
     </div>
 
     <div class="hero-body">
-        <div class="chat box">
-          <ul>
-            <li class="clearfix">
-              <div class="message-data align-right">
-                <span class="message-data-time">10:10 AM, Today</span> &nbsp;
-                &nbsp; <span class="message-data-name">Olia</span>
-                <i class="fa fa-circle me"></i>
-              </div>
-              <div class="message other-message float-right">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea commodi suscipit maiores iusto doloribus sed dicta tempora inventore repudiandae voluptate iste perferendis, voluptatum quas cupiditate doloremque repellat quaerat pariatur mollitia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ab nobis libero explicabo necessitatibus neque quod mollitia laudantium amet dignissimos debitis aliquid, sequi praesentium eaque suscipit saepe. Laboriosam, aut iusto!
-              </div>
-            </li>
-
-            <li>
-              <div class="message-data">
-                <span class="message-data-name"
-                  ><i class="fa fa-circle online"></i> Vincent</span
-                >
-                <span class="message-data-time">10:12 AM, Today</span>
-              </div>
-              <div class="message my-message">
-                Are we meeting today? Project has been already finished and I
-                have results to show you.
-              </div>
-            </li>
-
-            <li class="clearfix">
-              <div class="message-data align-right">
-                <span class="message-data-time">10:14 AM, Today</span> &nbsp;
-                &nbsp; <span class="message-data-name">Olia</span>
-                <i class="fa fa-circle me"></i>
-              </div>
-              <div class="message other-message float-right">
-                Well I am not sure. The rest of the team is not here yet. Maybe
-                in an hour or so? Have you faced any problems at the last phase
-                of the project?
-              </div>
-            </li>
-
-            <li>
-              <div class="message-data">
-                <span class="message-data-name"
-                  ><i class="fa fa-circle online"></i> Vincent</span
-                >
-                <span class="message-data-time">10:20 AM, Today</span>
-              </div>
-              <div class="message my-message">
-                Actually everything was fine. I'm very excited to show this to
-                our team.
-              </div>
-            </li>
-
-            <li>
-              <div class="message-data">
-                <span class="message-data-name"
-                  ><i class="fa fa-circle online"></i> Vincent</span
-                >
-                <span class="message-data-time">10:31 AM, Today</span>
-              </div>
-              <i class="fa fa-circle online"></i>
-              <i class="fa fa-circle online" style="color: #aed2a6"></i>
-              <i class="fa fa-circle online" style="color: #dae9da"></i>
-            </li>
-          </ul>
-        </div>
+      <div class="chat box">
+        <p class="chat-message is-me">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          explicabo dolorum dolor voluptates, provident at autem nesciunt saepe
+          a eum iusto, quis expedita labore voluptate quasi sapiente esse hic
+          praesentium! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Consectetur deserunt nulla, placeat rerum aperiam doloremque
+          accusantium necessitatibus officiis dolorem corrupti hic qui incidunt
+          tenetur aliquam laudantium possimus vero, dolore eveniet!
+          <span class="chat-name">Roma, {{ '10:55' }}</span>
+        </p>
+        <p class="chat-message is-me">
+          Lorem ipsum dolor sit amet consectetur adipisici
+          <span class="chat-name">Roma, {{ '10:55' }}</span>
+        </p>
+        <p class="chat-message">
+          Lorem ipsu eveniet!
+          <span class="chat-name">Asdf, {{ '10:57' }}</span>
+        </p>
       </div>
+    </div>
     <div class="hero-foot">
       <footer class="my-2 mx-5">
         <div class="chatinput field has-addons">
@@ -201,134 +156,23 @@ export default {
   height: 70vh;
 }
 
-.people-list {
-  width: 260px;
-  float: left;
-
-  .search {
-    padding: 20px;
-  }
-
-  .fa-search {
-    position: relative;
-    left: -25px;
-  }
-
-  ul {
-    padding: 20px;
-    height: 770px;
-
-    li {
-      padding-bottom: 20px;
-    }
-  }
-
-  img {
-    float: left;
-  }
-
-  .about {
-    float: left;
-    margin-top: 8px;
-  }
-
-  .about {
-    padding-left: 8px;
-  }
-}
-
-.chat-header {
-  padding: 20px;
-  border-bottom: 2px solid white;
-
-  img {
-    float: left;
-  }
-
-  .chat-about {
-    float: left;
-    padding-left: 10px;
-    margin-top: 6px;
-  }
-
-  .chat-with {
-    font-weight: bold;
-    font-size: 16px;
-  }
-
-
-  .fa-star {
-    float: right;
-    color: #d8dadf;
-    font-size: 20px;
-    margin-top: 12px;
-  }
-}
-
-.chat-history {
-  padding: 30px 30px 20px;
-  border-bottom: 2px solid white;
-  overflow-y: scroll;
-  height: 575px;
-
-  .message-data {
-    margin-bottom: 15px;
-  }
-
-  .message-data-time {
-    padding-left: 6px;
-  }
-
-  .message {
-    color: white;
-    padding: 18px 20px;
-    line-height: 26px;
-    font-size: 16px;
-    border-radius: 7px;
-    margin-bottom: 30px;
-    width: 90%;
-    position: relative;
-  }
-
-  .other-message {
-    &:after {
-      left: 93%;
-    }
-  }
-}
-
 .chat-message {
-  padding: 30px;
-
-  
-
-  .fa-file-o,
-  .fa-file-image-o {
-    font-size: 16px;
-    color: gray;
-    cursor: pointer;
-  }
-
-
+  position: relative;
+  background-color: #8281cf;
+  color: white;
+  width: fit-content;
+  border-radius: 10px;
+  padding: 5px;
+  margin: 0.7em;
+}
+.chat-name {
+  color: black;
+  font-size: 0.8rem;
+  margin-left:10px;
 }
 
-.online,
-.offline,
-.me {
-  margin-right: 3px;
-  font-size: 10px;
+.is-me {
+  margin-left: auto;
+  background-color: #aa237d;
 }
-
-.align-left {
-  text-align: left;
-}
-
-.align-right {
-  text-align: right;
-}
-
-.float-right {
-  float: right;
-}
-
 </style>
